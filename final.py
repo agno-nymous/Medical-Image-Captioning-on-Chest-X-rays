178276fb56ea13b3aaca27b35abc264d56505fc0
ashishthomaschempolil <ashishthomas7@gmail.com>
2021-02-09 16:39:00 -0500

    added test_images

--- final.py
+++ final.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import os
 import numpy as np
 import time
 from PIL import Image
@@ -6,12 +7,20 @@ import create_model as cm
 
 
 st.title("Chest X-ray Report Generator")
-st.text("by Ashish Thomas Chempolil")
+
+st.markdown("<small>by Ashish</small>",unsafe_allow_html=True)
+st.markdown("[<small>Github</small>](https://github.com/ashishthomaschempolil/Medical-Image-Captioning-on-Chest-X-rays)          [<small>Towards Data Science</small>](https://towardsdatascience.com/medical-image-captioning-on-chest-x-rays-a43561a6871d)",
+unsafe_allow_html=True)
+st.markdown("\nThis app will generate impression part of an X-ray report.\nYou can upload 2 X-rays that are front view and side view of chest of the same individual.")
+st.markdown("The 2nd X-ray is optional.")
+
+
+col1,col2 = st.beta_columns(2)
 image_1 = st.file_uploader("X-ray 1",type=['png','jpg','jpeg'],)
-image_2 = None
-if image_1:
-    image_2 = st.file_uploader("X-ray 2 (optional)",type=['png','jpg','jpeg'])
-predict_button = st.button('Predict')
+image_2 = st.file_uploader("X-ray 2 (optional)",type=['png','jpg','jpeg'])
+
+predict_button = col1.button('Predict on uploaded files')
+test_data = col2.button('Predict on sample data')
 
 @st.cache
 def create_model():
@@ -21,6 +30,7 @@ def create_model():
 
 def predict(image_1,image_2,model_tokenizer,predict_button = predict_button):
     start = time.process_time()
+    col1,col2 = st.beta_columns(2)
     if predict_button:
         if (image_1 is not None):
             start = time.process_time()  
@@ -32,7 +42,7 @@ def predict(image_1,image_2,model_tokenizer,predict_button = predict_button):
             else:
                 image_2 = Image.open(image_2).convert("RGB") #converting to 3 channels
                 image_2 = np.array(image_2)/255
-            st.image([image_1,image_2],width=256)
+            st.image([image_1,image_2],width=300)
             caption = cm.function1([image_1],[image_2],model_tokenizer)
             st.markdown(" ### **Impression:**")
             impression = st.empty()
@@ -43,9 +53,28 @@ def predict(image_1,image_2,model_tokenizer,predict_button = predict_button):
         else:
             st.markdown("## Upload an Image")
 
+def predict_sample(model_tokenizer,folder = './test_images'):
+    no_files = len(os.listdir(folder))
+    file = np.random.randint(1,no_files)
+    file_path = os.path.join(folder,str(file))
+    if len(os.listdir(file_path))==2:
+        image_1 = os.path.join(file_path,os.listdir(file_path)[0])
+        image_2 = os.path.join(file_path,os.listdir(file_path)[1])
+        print(file_path)
+    else:
+        image_1 = os.path.join(file_path,os.listdir(file_path)[0])
+        image_2 = image_1
+    predict(image_1,image_2,model_tokenizer,True)
+    
+
 model_tokenizer = create_model()
 
-predict(image_1,image_2,model_tokenizer)
+
+
+if test_data:
+    predict_sample(model_tokenizer)
+else:
+    predict(image_1,image_2,model_tokenizer)
 
 
 
